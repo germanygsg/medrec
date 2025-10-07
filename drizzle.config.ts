f0193173e8b7bf18993948065c3f26503a41351d
germanygsg <germanygsg@gmail.com>
2025-10-07 10:36:04 +0700

    fix deploy v4

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,5 @@ import type { Config } from 'drizzle-kit';
 export default {
     out: './drizzle',
     schema: './db/schema/*',
-    dialect: 'postgresql',
-    dbCredentials: {
-        connectionString: process.env.DATABASE_URL!,
-    },
+    connectionString: process.env.DATABASE_URL!,
 } satisfies Config;
